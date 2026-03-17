06e84369dc18fcaad6c093c6ba933e07f144d108
Julianna Iodo <julianna.i@firebirdtours.com>
2026-03-17 15:47:59 +0300

    Prepare for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Замените <имя_репозитория> на имя вашего GitHub репозитория
 export default defineConfig({
   plugins: [react()],
+  base: '/Movies/', // <-- эта строка аналогична "homepage" в CRA
 })
